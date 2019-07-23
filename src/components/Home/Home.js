5f6af84d4911b768bd33f869c431109ec36defaa
Mark Okafor <okaformark@gmail.com>
2019-07-22 20:00:11 -0500

    all donations print

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import firebase from 'firebase/app';
+import 'firebase/auth';
+import donationsData from '../../helpers/data/donationsData';
+import Donations from '../Donations/Donations';
+// import MyDonations from '../MyDonations/MyDonations';
+// import MyDonations from '../MyDonations/MyDonations';
 
 class Home extends React.Component {
+  state = {
+    donations: [],
+  }
+
+  getDonations = () => {
+    // const { uid } = firebase.auth().currentUser;
+    donationsData.getAllDonations()
+      .then(donations => this.setState({ donations }))
+      .catch(err => console.error('could not get donations', err));
+  }
+
+  componentDidMount() {
+    this.getDonations();
+  }
+
   render() {
+    const { donations } = this.state;
+    const makeDonationsCard = donations.map(donation => (
+      <Donations donation={donation} key={donation.id}/>
+    ));
     const profileLink = '/my-profile/789';
     return (
       <div className="Home">
         <h1>HOME</h1>
+        <div className="">
+        <h1>All Donations</h1>
+          {makeDonationsCard}
+        </div>
         <Link className="btn btn-info" to={profileLink}>Profile</Link>
       </div>
     );
--- src/components/MyDonations/MyDonations.js
+++ src/components/MyDonations/MyDonations.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Card,
+  // CardImg,
+  CardText,
+  CardBody,
+  // CardLink,
+  CardTitle,
+  CardSubtitle,
+}
+  from 'reactstrap';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import donationsData from '../../helpers/data/donationsData';
+// import Donations from '../Donations/Donations';
 
 class MyDonations extends React.Component {
+  state = {
+    myDonations: [],
+  }
+
+  getMyDonation = () => {
+    const { uid } = firebase.auth().currentUser;
+    donationsData.getMyDonations(uid)
+      .then(myDonationsPromise => this.setState({ myDonations: myDonationsPromise }))
+      .catch(err => console.error('could not get my donations', err));
+  }
+
+  componentDidMount() {
+    this.getMyDonation();
+  }
+
   render() {
+    const { myDonations } = this.state;
+    const selectedLink = `/my-donations/${myDonations.id}`;
+    // eslint-disable-next-line
+    // const image = require(`${myDonations.foodImageUrl}`);
+    const makeMyDonationsCard = myDonations.map(eachDonation => (
+      <Card>
+          <CardBody>
+            <CardTitle>{eachDonation.eventType}</CardTitle>
+            <CardSubtitle>{eachDonation.pickUpLocation}</CardSubtitle>
+          </CardBody>
+          {/* <img width="100%" height="auto" src={image} alt="" /> */}
+          <CardBody>
+            <CardText>{eachDonation.foodDescription}</CardText>
+            <button className="btn btn-outline-danger">X</button>
+            <Link className="btn btn-info" to={selectedLink} >View</Link>
+          </CardBody>
+        </Card>
+    ));
     return (
       <div className="MyDonations">
         <h1>My Donations</h1>
+        {makeMyDonationsCard}
       </div>
     );
   }
